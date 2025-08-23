cd303736816838d89216841de0d1d403145d0824
筱序二十 <2907713872@qq.com>
2025-08-23 20:55:21 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ const fonts = [
 ];
 
 export const defaultConfig = {
-    text: '梦爱吃鱼',       // 默认文本
-    watermark: '@梦爱吃鱼', // 默认水印
+    text: '筱序二十',       // 默认文本
+    watermark: '@青序栈', // 默认水印
     fontFamily: import.meta.env.VITE_APP_FONT_FAMILY,
     fontStyles: fonts.map(f => f[2]).filter(Boolean),
     fontOptions: fonts.map(([value, label]) => ({ value, label }))
